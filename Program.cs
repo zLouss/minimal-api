291c30812720caced68c2d098c1f4865b910d40b
Luis Victor Belo <luis.victor.belo@gmail.com>
2024-12-10 20:21:34 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+
+
